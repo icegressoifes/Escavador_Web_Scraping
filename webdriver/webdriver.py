1734189e3196cbb591000263fefe696e5bc2f66f
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2020-10-09 21:21:46 -0300

    Atualiza arquivo web driver

--- webdriver/webdriver.py
+++ webdriver/webdriver.py
@@ -26,8 +26,7 @@ def f_open_browser(driver=None, proxy=None):
 		options = webdriver.ChromeOptions()
 		userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36' if platform.system() == "Windows" else "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
 		options.add_argument('user-agent={}'.format(userAgent))	
-		options.add_argument("start-maximized")
-		options.add_argument('window-size=1900,1080')    
+		options.add_argument("start-maximized")    
 		# options.add_argument('--disable-gpu')
 		# options.add_argument('--disable-extensions')
 		# options.add_argument("--disable-dev-shm-usage")
@@ -35,11 +34,9 @@ def f_open_browser(driver=None, proxy=None):
 		options.add_argument("--user-data-dir=browser_cache")
 		options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
 		options.add_experimental_option('useAutomationExtension', False)
-		prefs = {"profile.password_manager_enabled": True, "credentials_enable_service": True}
-		options.add_experimental_option("prefs", prefs)  
-		# driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME, options=options)
-		diretorio = './chromedriver.exe' if platform.system() == "Windows" else "./chromedriver"
-		driver = webdriver.Chrome(options=options, executable_path=r'{}'.format(diretorio))
+		driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME, options=options)
+		# diretorio = './chromedriver.exe' if platform.system() == "Windows" else "./chromedriver"
+		# driver = webdriver.Chrome(options=options, executable_path=r'{}'.format(diretorio))
 		return driver
 	except:
 		
