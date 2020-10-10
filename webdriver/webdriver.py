7489cffe32b68dbe3d85cc01853506fa7e5240e2
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2020-10-10 05:16:12 -0300

    atualiza

--- webdriver/webdriver.py
+++ webdriver/webdriver.py
@@ -26,7 +26,8 @@ def f_open_browser(driver=None, proxy=None):
 		options = webdriver.ChromeOptions()
 		userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36' if platform.system() == "Windows" else "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
 		options.add_argument('user-agent={}'.format(userAgent))	
-		options.add_argument("start-maximized")    
+		# options.add_argument("start-maximized")
+		options.add_argument('window-size=1280,720')
 		# options.add_argument('--disable-gpu')
 		# options.add_argument('--disable-extensions')
 		# options.add_argument("--disable-dev-shm-usage")
